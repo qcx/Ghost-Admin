719c7d0c085b32386528cf9c46edea858f53ea38
Lucas Monteiro <lucashtm@id.uff.br>
2019-10-17 17:31:34 -0300

    Dynamic odin associations dropdown

--- app/components/gh-psm-associations-input.js
+++ app/components/gh-psm-associations-input.js
@@ -1,36 +1,45 @@
 import Ember from 'ember';
-import {computed} from '@ember/object';
-
+const brokkUrl = 'https://vqs32o851c.execute-api.us-east-1.amazonaws.com/production/';
 export default Ember.Component.extend({
-  selectedModule: null,
-  selectedRecord: null,
-  modules: [
-    {
+  examiningBoards: null,
+  selectedExaminingBoards: [],
+  examiningBoardsIds: [],
+  institutes: null,
+  selectedInstitutes: [],
+  institutesIds: [],
+  examiningBoard: {
       id: 4,
-      name: 'Banca'
+      name: 'Examining Board',
+      getFunction: 'getExaminingBoards'
     },
-    {
-      id: 6,
-      name: 'Cargo'
-    },
-    {
+  institute: {
       id: 11,
-      name: 'Instituto'
-    }
-  ],
-  moduleOptions: [],
-  options: {
-    4: ['A', 'B', 'C'],
-    6: ['1', '2', '3'],
-    11: ['X', 'Y', 'Z']
-  },
-  actions: {
-    setModule: function(selected) {
-      this.set('selectedModule', selected)
-      this.set('moduleOptions', this.get('options')[selected.id])
+      name: 'Institute',
+      getFunction: 'getInstitutes'
     },
-    isModulePresent: function() {
-      return selectedModule !== null;
+  actions: {
+    selectExaminingBoard(selected){
+      selected = selected.reduce((unique, element) => {
+        return unique.map(e => e.id).includes(element.id) ? unique : [...unique, element];
+      }, []);
+      this.set('selectedExaminingBoards', selected);
+    },
+    selectInstitute(selected){
+      selected = [...new Set(selected)];
+      this.set('selectedInstitutes', this.selectedInstitutes.concat(selected));
+      this.set('institutesIds', this.institutesIds.concat([selected[0].id]));
+    },
+    getExaminingBoards() {
+      if(!this.examiningBoards){
+        let collection = fetch(brokkUrl + 'product/1/examining_boards').then(response => response.json()).then(data => data);
+        this.set('examiningBoards', collection);
+      }
+    },
+    getInstitutes() {
+      if(!this.institutes){
+        let collection = fetch(brokkUrl + 'product/1/institutes').then(response => response.json()).then(data => data);
+        this.set('institutes', collection);
+      }
     }
   }
 });
